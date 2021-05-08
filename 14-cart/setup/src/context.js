92b9165e2c54e80622cc2def7318b19c10e1e4ac
UJ528491 <hyj0829@gmail.com>
2021-05-09 02:57:26 +0900

    add total

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -10,7 +10,7 @@ const initialState = {
   loading: false,
   cart: cartItems,
   total: 0,
-  amount: 0,
+  allAmount: 0,
 };
 
 const AppProvider = ({ children }) => {
@@ -21,12 +21,24 @@ const AppProvider = ({ children }) => {
   const remove = id => {
     dispatch({ type: "REMOVE", payload: id });
   };
+  const increase = id => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = id => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+    console.log(state);
+  }, [state.cart]);
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
         remove,
+        increase,
+        decrease,
       }}
     >
       {children}
