9e7922fca8db65388c564ff4d3a1a13124d5264d
UJ528491 <hyj0829@gmail.com>
2021-07-09 01:16:12 +0900

    18-pagination

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,67 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  useEffect(() => {}, []);
+  const handlePages = e => {
+    if (e.target.classList.contains("prev-btn")) {
+      if (page === 0) {
+        setPage(data.length - 1);
+      } else {
+        setPage(page - 1);
+      }
+    }
+    if (e.target.classList.contains("next-btn")) {
+      if (page === data.length - 1) {
+        setPage(0);
+      } else {
+        setPage(page + 1);
+      }
+    }
+  };
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagenation"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {loading
+            ? "loading..."
+            : data[page].map(person => {
+                const { avatar_url, login, repos_url } = person;
+                const follow = {
+                  image: avatar_url,
+                  name: login,
+                  url: repos_url,
+                };
+                return <Follower {...follow} />;
+              })}
+        </div>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={e => handlePages(e)}>
+            prev
+          </button>
+          {data.map((_, index) => {
+            return (
+              <button
+                className={`page-btn ${page === index ? "active-btn" : "null"}`}
+                onClick={() => setPage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button className="next-btn" onClick={e => handlePages(e)}>
+            next
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 18-pagination/setup/src/Follower.js
+++ 18-pagination/setup/src/Follower.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Follower = () => {
-  return <h2>follower component</h2>
-}
+const Follower = ({ image, name, url }) => {
+  return (
+    <article className="card">
+      <img src={image} alt="thum-nail" />
+      <h4>{name}</h4>
+      <a href={url} className="btn">
+        view profile
+      </a>
+    </article>
+  );
+};
 
-export default Follower
+export default Follower;
