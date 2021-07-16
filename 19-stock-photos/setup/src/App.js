caca389e8c51fbf4251884fd5c070a58bb48eab5
UJ528491 <hyj0829@gmail.com>
2021-07-16 12:44:44 +0900

    19-stock-photos

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,100 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(true);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const fetchApi = async () => {
+    setLoading(true);
+    let url;
+    if (query) {
+      url = `${searchUrl}${clientID}&page=${page}&query=${query}`;
+    } else {
+      url = `${mainUrl}${clientID}&page=${page}`;
+    }
+    try {
+      const response = await fetch(url);
+      let data = await response.json();
+      if (query) {
+        data = data.results;
+      }
+      const newData = data.map(item => {
+        return {
+          image: item.urls.raw,
+          name: item.user.name,
+          likes: item.likes,
+          thumb: item.user.profile_image.large,
+          link: item.user.portfolio_url,
+          alt: item.alt_description,
+        };
+      });
+      if (query && page === 1) {
+        setPhotos(newData);
+      } else {
+        setPhotos([...photos, ...newData]);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchApi();
+    console.log("f5");
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      const scrollEnd =
+        window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+      if (scrollEnd) {
+        console.log("end");
+        setPage(page + 1);
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+  });
+  const handleSubmit = e => {
+    e.preventDefault();
+    setPage(1);
+    fetchApi();
+  };
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            className="form-input"
+            placeholder="search"
+            // value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+          <button className="submit-btn" type="submit" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((item, index) => (
+            <Photo key={index} photos={item} />
+          ))}
+        </div>
+        {loading && <h1 className="loading">Loading...</h1>}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({ photos }) => {
+  const { image, name, likes, thumb, link, alt } = photos;
+  return (
+    <article className="photo">
+      <img src={image} alt={alt} />
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={link}>
+          <img className="user-img" src={thumb} alt="" />
+        </a>
+      </div>
+    </article>
+  );
+};
 
-export default Photo
+export default Photo;
